1a0d33b981119b45213b88d6e8a9e105671d55c2
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-10-01 19:05:42 -0500

    Added authentication

--- whisper.go
+++ whisper.go
@@ -33,6 +33,13 @@ var (
 	DBDataSourceName = fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", DBHost, DBPort, DBUser, DBPasswd, DB)
 )
 
+func genAuth() string {
+	auth := make([]byte, 64)
+	rand.Seed(time.Now().UnixNano())
+	rand.Read(auth)
+	return hex.EncodeToString(auth)
+}
+
 func NewUser(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("postgres", DBDataSourceName)
 	if err != nil {
@@ -41,16 +48,36 @@ func NewUser(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	name := r.Header.Get("Name")
-	rand.Seed(time.Now().UnixNano())
-	authHashSeed := make([]byte, 64)
-	rand.Read(authHashSeed)
-	authHashBytes := sha512.Sum512(authHashSeed)
-	authHash := hex.EncodeToString(authHashBytes[:])
-	db.Exec(fmt.Sprintf("insert into users (name, authhash) values ('%s', '%s');", name, authHash))
+	auth := genAuth()
+	fmt.Fprintln(w, auth)
+	authHash := sha512.Sum512([]byte(auth))
+	db.Exec(fmt.Sprintf("insert into users (name, authhash) values ('%s', '%s');", name, hex.EncodeToString(authHash[:])))
+}
+
+func Me(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", DBDataSourceName)
+	if err != nil {
+		fmt.Println("Failed to connect to db")
+	}
+	defer db.Close()
+
+	name := r.Header.Get("Name")
+	auth := r.Header.Get("Auth")
+	authHash := sha512.Sum512([]byte(auth))
+	q, err := db.Query(fmt.Sprintf("select * from users where name='%s' and authhash='%s';", name, hex.EncodeToString(authHash[:])))
+	if err != nil {
+		fmt.Println("Query failed")
+	}
+	if q.Next() {
+		fmt.Fprintf(w, "Auth Success\n")
+	} else {
+		fmt.Fprintf(w, "Failed to Authenticate as: %s, with Auth: %s\n", name, auth)
+	}
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 	http.HandleFunc("/newuser", NewUser)
+	http.HandleFunc("/me", Me)
 	http.ListenAndServe(":3000", nil)
 }
