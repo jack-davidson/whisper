5989ec1d2c64ec493061ac9be7568c384fb4a322
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-10-02 14:34:19 -0500

    moved away from token auth

--- whisper.go
+++ whisper.go
@@ -45,13 +45,32 @@ func hash(s string) string {
 	return hex.EncodeToString(authHash[:])
 }
 
+func lookupUserID(name string) *string {
+	db, err := sql.Open("postgres", DBDataSourceName)
+	if err != nil {
+		fmt.Println("Failed to connect to db")
+	}
+	defer db.Close()
+	q, err := db.Query("select id from users where name=$1;", name)
+	if err != nil {
+		fmt.Println(err.Error())
+		return nil
+	}
+	if q.Next() {
+		var id string
+		q.Scan(&id)
+		return &id
+	}
+	return nil
+}
+
 func authenticate(name string, auth string, then func(db *sql.DB, id string)) {
 	db, err := sql.Open("postgres", DBDataSourceName)
 	if err != nil {
 		fmt.Println("Failed to connect to db")
 	}
 	defer db.Close()
-	q, err := db.Query("select id from users where name=$1 and authhash=$2;", name, hash(auth))
+	q, err := db.Query("select id from users where name=$1 and passwordhash=$2;", name, hash(auth))
 	if err != nil {
 		fmt.Println(err.Error())
 		return
@@ -71,10 +90,11 @@ func NewUser(w http.ResponseWriter, r *http.Request) {
 	defer db.Close()
 
 	name := r.Header.Get("Name")
+	password := r.Header.Get("Password")
 	q, err := db.Query("select * from users where name=$1;", name)
 	if !q.Next() {
 		auth := genHash()
-		db.Exec("insert into users (name, authhash) values ($1, $2);", name, hash(auth))
+		db.Exec("insert into users (name, passwordhash) values ($1, $2);", name, hash(password))
 		fmt.Fprintln(w, auth)
 	} else {
 		w.WriteHeader(http.StatusConflict)
@@ -82,13 +102,13 @@ func NewUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Request) {
-	authenticate(r.Header.Get("Name"), r.Header.Get("Auth"), func(db *sql.DB, id string) {
+	authenticate(r.Header.Get("Name"), r.Header.Get("Password"), func(db *sql.DB, id string) {
 		db.Exec("delete from users where id=$1;", id)
 	})
 }
 
 func Messages(w http.ResponseWriter, r *http.Request) {
-	authenticate(r.Header.Get("Name"), r.Header.Get("Auth"), func(db *sql.DB, id string) {
+	authenticate(r.Header.Get("Name"), r.Header.Get("Password"), func(db *sql.DB, id string) {
 		q, err := db.Query("select * from messages where foruser=$1;", id)
 		if err != nil {
 			fmt.Println(err.Error())
@@ -103,10 +123,18 @@ func Messages(w http.ResponseWriter, r *http.Request) {
 }
 
 func Message(w http.ResponseWriter, r *http.Request) {
-	authenticate(r.Header.Get("Name"), r.Header.Get("Auth"), func(db *sql.DB, id string) {
+	authenticate(r.Header.Get("Name"), r.Header.Get("Password"), func(db *sql.DB, id string) {
 		text := r.Header.Get("Text")
-		db.Exec("insert into messages (text, foruser, fromuser) values ($1, $2, $3);", text, id, id)
+		forUserID := r.Header.Get("For")
+		db.Exec("insert into messages (text, foruser, fromuser) values ($1, $2, $3);", text, forUserID, id)
 	})
+}
+
+func User(w http.ResponseWriter, r *http.Request) {
+	id := lookupUserID(r.Header.Get("Name"))
+	if id != nil {
+		fmt.Fprintln(w, *id)
+	}
 }
 
 func main() {
@@ -115,5 +143,6 @@ func main() {
 	http.HandleFunc("/deleteuser", DeleteUser)
 	http.HandleFunc("/messages", Messages)
 	http.HandleFunc("/message", Message)
+	http.HandleFunc("/user", User)
 	http.ListenAndServe(":3000", nil)
 }
