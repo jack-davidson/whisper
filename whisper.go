08921bbbe45e2cc9ec0d747c252630508e4ea754
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-10-02 13:33:55 -0500

    sending messages

--- whisper.go
+++ whisper.go
@@ -58,6 +58,60 @@ func NewUser(w http.ResponseWriter, r *http.Request) {
 	db.Exec("insert into users (name, authhash) values ($1, $2);", name, hash(auth))
 }
 
+func Messages(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", DBDataSourceName)
+	if err != nil {
+		fmt.Println("Failed to connect to db")
+	}
+	defer db.Close()
+
+	name := r.Header.Get("Name")
+	auth := r.Header.Get("Auth")
+
+	q, err := db.Query("select id from users where name=$1 and authhash=$2;", name, hash(auth))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if q.Next() {
+		var id string
+		q.Scan(&id)
+		q, err := db.Query("select * from messages where foruser=$1;", id)
+		if err != nil {
+			fmt.Println(err.Error())
+			return
+		}
+		for q.Next() {
+			var id, text, foruser, fromuser interface{}
+			q.Scan(&id, &foruser, &fromuser, &text)
+			fmt.Fprintf(w, "%s '%s' %s %s\n", id, text, foruser, fromuser)
+		}
+	}
+}
+
+func Message(w http.ResponseWriter, r *http.Request) {
+	db, err := sql.Open("postgres", DBDataSourceName)
+	if err != nil {
+		fmt.Println("Failed to connect to db")
+	}
+	defer db.Close()
+
+	name := r.Header.Get("Name")
+	auth := r.Header.Get("Auth")
+	text := r.Header.Get("Text")
+
+	q, err := db.Query("select id from users where name=$1 and authhash=$2;", name, hash(auth))
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	if q.Next() {
+		var fromuser string
+		q.Scan(&fromuser)
+		db.Exec("insert into messages (text, foruser, fromuser) values ($1, $2, $3);", text, fromuser, fromuser)
+	}
+}
+
 func Me(w http.ResponseWriter, r *http.Request) {
 	db, err := sql.Open("postgres", DBDataSourceName)
 	if err != nil {
@@ -84,6 +138,8 @@ func Me(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
 	http.HandleFunc("/newuser", NewUser)
+	http.HandleFunc("/messages", Messages)
+	http.HandleFunc("/message", Message)
 	http.HandleFunc("/me", Me)
 	http.ListenAndServe(":3000", nil)
 }
